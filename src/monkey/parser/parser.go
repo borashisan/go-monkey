193105a59b1576df149339266cd856d879e0b828
borashisan <tomochuro731@gmail.com>
2022-08-03 14:03:20 +0900

    整数リテラル

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"monkey/lexer"
 	"monkey/token"
   "fmt"
+  "strconv"
 )
 
 type (
@@ -31,6 +32,7 @@ func New(l *lexer.Lexer) *Parser {
 
   p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
   p.registerPrefix(token.IDENT, p.parseIdentifier)
+  p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// 2つのトークンを読み込む。curTokenとpeekTokenの両方がセットされる。
 	p.nextToken()
@@ -146,6 +148,20 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
   leftExp := prefix()
 
   return leftExp
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+  lit := &ast.IntegerLiteral{Token: p.curToken}
+
+  value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+  if err != nil {
+    msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+    p.errors = append(p.errors, msg)
+    return nil
+  }
+  lit.Value = value
+
+  return lit
 }
 
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
