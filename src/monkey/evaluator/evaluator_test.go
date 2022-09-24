91047d859fa3181a9507752f87b878b26c2907ea
borashisan <tomochuro731@gmail.com>
2022-09-24 09:59:02 +0900

    文字列結合

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -49,6 +49,20 @@ func TestStringLiteral(t *testing.T) {
   }
 }
 
+func TestStringConnection(t *testing.T) {
+  input := `"Hello" + " " + "World!"`
+
+  evaluated := testEval(input)
+  str, ok := evaluated.(*object.String)
+  if !ok {
+    t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+  }
+
+  if str.Value != "Hello World!" {
+    t.Errorf("String has wrong value. got=%q", str.Value)
+  }
+}
+
 func TestEvalBooleanExpression(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -187,6 +201,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+    {
+      `"Hello" - "World"`,
+      "unknown operator: STRING - STRING",
+    },
 		{
 			`
 if (10 > 1) {
